e7fc3068a0b4681829e4069ca77da06e32d7601b
Néstor Ibarra <nstor7@hotmail.com>
2018-05-02 16:31:19 -0500

    agregando json estructurados inicio en el backend de la app

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const tienda = require('./tienda')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(bodyParser.json())
+
 app.set('view engine', 'pug')
 
 app.use(express.static('public'))
@@ -25,8 +27,41 @@ app.get('/', function(req, res){
    title: 'Folk in Love - Investigación y Difusión del Folklore de Panamá',
    description: 'En Folk in love nos dedicamos a la investigación, difusión, promoción y conservación del folklore Panameño.',
    keywords: 'folklore panameño, costumbres, tradidiones, panama, orígenes, Panamá',
-   estructurados: 'https://www.folkinlovepty.com/json/estructurados-inicio.json'
+   estructurados: 'https://www.folkinlovepty.com/json/estructurados-inicio'
  })
+})
+app.get('/estructurados-inicio', function(req, res){
+  res.send(
+    {
+      "@context": "http://schema.org",
+      "@type": "NewsArticle",
+      "mainEntityOfPage": {
+        "@type": "WebPage",
+        "@id": "https://folkinlovepty.com/"
+      },
+      "headline": "Article headline",
+      "image": [
+        "https://folkinlovepty.com/images/trajes-tipicos-panama-banner-full-2x.jpg",
+        "https://folkinlovepty.com/images/moneda-coronada-full.jpg",
+        "https://folkinlovepty.com/images/pollera-gala-ocu-identidad-full.jpg"
+       ],
+      "datePublished": "2017-10-016T00:00:00+00:00",
+      "dateModified": "2018-05-02T09:20:00+08:00",
+      "author": {
+        "@type": "Person",
+        "name": "Cinthia González"
+      },
+       "publisher": {
+        "@type": "Organization",
+        "name": "Folk in Love",
+        "logo": {
+          "@type": "ImageObject",
+          "url": "https://folkinlovepty.com/images/folkInLove-logo.png"
+        }
+      },
+      "description": "A most wonderful article"
+     }
+  )
 })
 app.get('/contacto', function(req,res){
  res.render('index.pug')
