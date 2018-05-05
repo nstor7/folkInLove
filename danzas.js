3a75256acc7942fb1fe5384d9d3ef1dd89f774b7
Néstor Ibarra <nstor7@hotmail.com>
2018-05-05 15:56:39 -0500

    schema para danzas

--- danzas.js
+++ danzas.js
@@ -9,7 +9,12 @@ danzas.get('/', function(req, res){
   link: 'https://www.folkinlovepty.com/danzas/',
   title: 'Folk in Love - Bailes Típicos de Panamá',
   description: 'Los Bailes típicos de Panamá son uno de los elementos más importantes de la identidad panameña. Clasificación de las danzas folklóricas por región. ',
-  keywords: 'Danzas de Panamá, Bailes Típicos, Pasos de Baile'
+  keywords: 'Danzas de Panamá, Bailes Típicos, Pasos de Baile',
+  schemaType: 'Article',
+  schemaImages:['images/danzas-full.jpg'],
+  schemaPublished: '2017-10-016T00:00:00+00:00',
+  schemaModified: '2018-5-05T03:41:00+03:41',
+
   })
 })
 
